2f99160ff1e5d2a1bc4bbe55f2a56fd5966c3560
Sunny Basagare <114205101+Sunnyb636@users.noreply.github.com>
2024-03-07 16:40:18 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Install all the dependencies using apt-get
+sudo apt-get install -y \
+apparmor apport apport-gtk aptdaemon awscli chrome-gnome-shell command-not-found deja-dup duplicity \
+firefox foomatic-db-compressed-ppds gdm3 gedit gedit-common gitsome gnome-control-center gnome-getting-started-docs \
+gnome-menus gnome-online-accounts gnome-shell gnome-shell-extension-appindicator gnome-shell-extension-desktop-icons \
+gnome-shell-extension-prefs gnome-shell-extension-ubuntu-dock gnome-shell-extensions gnome-terminal gnome-tweaks \
+gnome-user-docs gvfs-backends hplip hplip-data ibus ibus-table language-selector-common language-selector-gnome \
+libsmbclient lsb-release mysql-apt-config nautilus-extension-gnome-terminal netplan.io networkd-dispatcher \
+openprinting-ppds orca plymouth-theme-spinner plymouth-theme-ubuntu-text printer-driver-foo2zjs \
+printer-driver-foo2zjs-common printer-driver-m2300w printer-driver-postscript-hp printer-driver-ptouch \
+printer-driver-pxljr printer-driver-sag-gdi python3 python3-alabaster python3-appdirs python3-apport python3-apt \
+python3-aptdaemon python3-aptdaemon.gtk3widgets python3-babel python3-bcrypt python3-blinker python3-botocore \
+python3-brlapi python3-cairo python3-certifi python3-cffi-backend python3-chardet python3-click python3-colorama \
+python3-commandnotfound python3-cryptography python3-cups python3-cupshelpers python3-dateutil python3-dbus \
+python3-debconf python3-debian python3-defer python3-distlib python3-distro python3-distro-info python3-distupgrade \
+python3-distutils python3-docopt python3-docutils python3-entrypoints python3-fasteners python3-feedparser \
+python3-filelock python3-future python3-gdbm python3-gi python3-gi-cairo python3-httplib2 python3-ibus-1.0 \
+python3-idna python3-imagesize python3-importlib-metadata python3-jinja2 python3-jmespath python3-jwt python3-keyring \
+python3-launchpadlib python3-lazr.restfulclient python3-lazr.uri python3-ldb python3-lib2to3 python3-lockfile \
+python3-louis python3-macaroonbakery python3-mako python3-markupsafe python3-monotonic python3-more-itertools \
+python3-nacl python3-netifaces python3-numpydoc python3-oauthlib python3-olefile python3-packaging python3-paramiko \
+python3-pexpect python3-pil python3-pip python3-pkg-resources python3-ply python3-problem-report python3-prompt-toolkit \
+python3-protobuf python3-ptyprocess python3-pyasn1 python3-pyatspi python3-pygments python3-pymacaroons python3-pyparsing \
+python3-renderpm python3-reportlab python3-reportlab-accel python3-requests python3-requests-unixsocket python3-rfc3339 \
+python3-roman python3-rsa python3-s3transfer python3-secretstorage python3-setproctitle python3-setuptools \
+python3-sigmavirus24-urltemplate python3-simplejson python3-six python3-speechd python3-sphinx python3-systemd \
+python3-talloc python3-tz python3-uno python3-update-manager python3-urllib3 python3-venv python3-virtualenv \
+python3-wadllib python3-wcwidth python3-wheel python3-xdg python3-xkit python3-yaml python3-zipp python3.8-venv \
+rhythmbox-plugin-alternative-toolbar rhythmbox-plugins samba-libs snapd ssh-import-id system-config-printer \
+system-config-printer-common system-config-printer-udev totem-plugins ubuntu-advantage-tools ubuntu-docs \
+ubuntu-drivers-common ubuntu-minimal ubuntu-release-upgrader-core ubuntu-release-upgrader-gtk ubuntu-session \
+ubuntu-standard ufw update-manager update-manager-core update-notifier update-notifier-common usb-creator-common \
+usb-creator-gtk virtualenv xonsh xorg xserver-xorg yelp
